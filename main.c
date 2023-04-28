22b3332d8e57dd1279f3759d05fd14ae55939266
BraianPerez97 <3603@holbertonschool.com>
2023-04-28 14:45:04 +0000

    updating comments of main.c file

--- main.c
+++ main.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
-  * main - simple UNIX interpreter
-  *
-  * Return: -1 if failure, 0 otherwise.
-  */
+ * main - simple UNIX interpreter
+ *
+ * Return: -1 if failure, 0 otherwise.
+ */
 
 int main(void)
 {
@@ -14,38 +14,63 @@ int main(void)
 
 	for (; 1; inputs++) /* Infinite loop which counts input lines */
 	{
+		/* cheks if the program is being executed in terminal */
 		isatty(STDIN_FILENO) == 1 ? write(1, "$ ", 2) : 0;
-		f = getline(&buf, &bufsize, stdin); /* user input line */
+		/* read user input */
+		f = getline(&buf, &bufsize, stdin);
+		/* handles case where getline returns -1)*/
 		if (f == -1)
 		{
 			free(buf);
-			return (status); }
-		if (_strcmp(buf, "\n") == 0) /* if only newline is passed */
+			return (status); 
+		}
+		/* handles if only newline is passed */
+		if (_strcmp(buf, "\n") == 0)
 		{
 			free(buf), buf = NULL;
-			continue; }
-		buf[f - 1] = '\0', argv = malloc(sizeof(char *)); /* replaces \n for \0 */
+			continue; 
+		}
+		/* replace newline character with a null terminator */
+		buf[f - 1] = '\0';
+	       /* allocate memory for the first argument of the command */	
+		argv = malloc(sizeof(char *));
+		/* handles the case where malloc returns NULL */
 		if (argv == NULL)
 		{
 			free(buf);
-			return (status); }
-		dup = _strdup(buf), token = strtok(dup, " \t"), argv[0] = token;
-		if (argv[0] == NULL || token == NULL) /* Above tokenize the first argument */
+			return (status); 
+		}
+		/* duplicate the input line */
+		dup = _strdup(buf);
+		/* tokenize the first argument of the command */
+		token = strtok(dup, " \t");
+	       	argv[0] = token;
+		/* handles case where the first argument is empty */ 
+		if (argv[0] == NULL || token == NULL)
 		{
 			free(argv), free(dup);
-			continue; }
-		for (i = 1; token; i++) /* Continues to tokenize */
-		{ /* Each token is part of the line using space or tab as delimiters */
+			continue; 
+		}
+		/* tokenize the rest of command argument */
+		for (i = 1; token; i++)
+		{
 			token = strtok(NULL, " \t");
 			argv = _realloc(argv, i * sizeof(char *), (i + 1) * sizeof(char *));
-			argv[i] = token; }
+			argv[i] = token; 
+		}
+		/* allocate memory of the NULL terminator at the end of argument */
 		argv = _realloc(argv, i * sizeof(char *), (i + 1) * sizeof(char *));
 		argv[i] = NULL;
 		if (_strcmp(argv[0], "env") == 0 && !argv[1]) /* env built-in */
 		{
 			free(argv), free(dup), _printenv();
-			continue; }
+			continue; 
+		}
 		if (_strcmp(argv[0], "exit") == 0 && !argv[1]) /* exit built-in */
 			free(argv), free(dup), free(buf), exit(status);
-		status = executor(argv, inputptr), free(dup); } /* Execute command */
-	return (status); } /* or prints error message if not found */
+		/* execute the command or print error */
+		status = executor(argv, inputptr);
+	       	free(dup);
+	}
+	return (status);
+}
